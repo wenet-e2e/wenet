4ecd9cf6cd1ca2ef52c2fecafbc2243eabe4a982
Di Wu <1176705630@qq.com>
2020-12-28 10:02:57 +0800

    [examples] add librispeech recipe (#30)
    [tools] specaugmentation config

--- tools/format_data.sh
+++ tools/format_data.sh
@@ -80,7 +80,7 @@ fi
 mkdir -p ${tmpdir}/output
 if [ -n "${bpecode}" ]; then
     paste -d " " <(awk '{print $1}' ${dir}/text) <(cut -f 2- -d" " ${dir}/text \
-        | spm_encode --model=${bpecode} --output_format=piece) \
+        | tools/spm_encode --model=${bpecode} --output_format=piece) \
         > ${tmpdir}/output/token.scp
 elif [ -n "${nlsyms}" ]; then
     tools/text2token.py -s 1 -n 1 -l ${nlsyms} ${dir}/text --trans_type ${trans_type} > ${tmpdir}/output/token.scp
