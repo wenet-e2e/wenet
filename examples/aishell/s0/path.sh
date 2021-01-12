d17e64e838d62f7879d5ccbe7912348a502fe815
Binbin Zhang <811364747@qq.com>
2021-01-12 12:47:58 +0000

    [fix] fix wav_to_duration bug and error info
    Change-Id: I8d1823219a370dd4c0adc3778b04d69be691be2d

--- examples/aishell/s0/path.sh
+++ examples/aishell/s0/path.sh
@@ -1,7 +1,5 @@
 export PATH=$PWD:$PATH
 
-export LC_ALL=C
-
 # NOTE(kan-bayashi): Use UTF-8 in Python to avoid UnicodeDecodeError when LC_ALL=C
 export PYTHONIOENCODING=UTF-8
 export PYTHONPATH=../../:$PYTHONPATH
