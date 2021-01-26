8412f189ba531d6035a1bcbecf636a2e81e93647
Chao Yang <385526069@qq.com>
2021-01-26 21:01:49 +0800

    [doc] refine comments (#100)
    * modify tutorial.md for torchaudio
    
    * remove trailing space
    
    * refine comments

--- wenet/transformer/positionwise_feed_forward.py
+++ wenet/transformer/positionwise_feed_forward.py
@@ -11,11 +11,14 @@ import torch
 class PositionwiseFeedForward(torch.nn.Module):
     """Positionwise feed forward layer.
 
+    FeedForward are appied on each position of the sequence.
+    The output dim is same with the input dim.
+
     Args:
         idim (int): Input dimenstion.
         hidden_units (int): The number of hidden units.
         dropout_rate (float): Dropout rate.
-
+        activation (torch.nn.Module): Activation function
     """
     def __init__(self,
                  idim: int,
@@ -25,10 +28,16 @@ class PositionwiseFeedForward(torch.nn.Module):
         """Construct a PositionwiseFeedForward object."""
         super(PositionwiseFeedForward, self).__init__()
         self.w_1 = torch.nn.Linear(idim, hidden_units)
-        self.w_2 = torch.nn.Linear(hidden_units, idim)
-        self.dropout = torch.nn.Dropout(dropout_rate)
         self.activation = activation
+        self.dropout = torch.nn.Dropout(dropout_rate)
+        self.w_2 = torch.nn.Linear(hidden_units, idim)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward function."""
-        return self.w_2(self.dropout(self.activation(self.w_1(x))))
+    def forward(self, xs: torch.Tensor) -> torch.Tensor:
+        """Forward function.
+
+        Args:
+            xs: input tensor (B, L, D)
+        Returns:
+            output tensor, (B, L, D)
+        """
+        return self.w_2(self.dropout(self.activation(self.w_1(xs))))
