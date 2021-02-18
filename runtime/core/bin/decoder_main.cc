d99b57878006044c1cff2170f5a2fe68c7aea8ed
彭震东 <275331498@qq.com>
2021-02-18 14:55:27 +0800

    [runtime] support both wave scp and single wave (#169)
    * [runtime] support both wave scp and single wave
    
    * [runtime] use vector and pair instead of unorder_map

--- runtime/core/bin/decoder_main.cc
+++ runtime/core/bin/decoder_main.cc
@@ -3,6 +3,7 @@
 
 #include <chrono>
 #include <iomanip>
+#include <utility>
 
 #include "gflags/gflags.h"
 #include "glog/logging.h"
@@ -21,6 +22,7 @@ DEFINE_int32(chunk_size, 16, "decoding chunk size");
 DEFINE_int32(num_threads, 1, "num threads for device");
 DEFINE_bool(simulate_streaming, false, "simulate streaming input");
 DEFINE_string(model_path, "", "pytorch exported model path");
+DEFINE_string(wav_path, "", "single wave path");
 DEFINE_string(wav_scp, "", "input wav scp");
 DEFINE_string(dict_path, "", "dict path");
 DEFINE_string(result, "", "result output file");
@@ -36,27 +38,39 @@ int main(int argc, char *argv[]) {
   decode_config.chunk_size = FLAGS_chunk_size;
   wenet::FeaturePipelineConfig feature_config;
   feature_config.num_bins = FLAGS_num_bins;
+  const int sample_rate = 16000;
+  auto feature_pipeline =
+      std::make_shared<wenet::FeaturePipeline>(feature_config);
+
+  if (FLAGS_wav_path.empty() && FLAGS_wav_scp.empty()) {
+    LOG(FATAL) << "Please provide the wave path or the wav scp.";
+  }
+  std::vector<std::pair<std::string, std::string>> waves;
+  if (!FLAGS_wav_path.empty()) {
+    waves.emplace_back(make_pair("test", FLAGS_wav_path));
+  } else {
+    std::ifstream wav_scp(FLAGS_wav_scp);
+    std::string line;
+    while (getline(wav_scp, line)) {
+      std::vector<std::string> strs;
+      wenet::SplitString(line, &strs);
+      CHECK_GE(strs.size(), 2);
+      waves.emplace_back(make_pair(strs[0], strs[1]));
+    }
+  }
 
   std::ofstream result;
-  result.open(FLAGS_result);
+  if (!FLAGS_result.empty()) {
+    result.open(FLAGS_result, std::ios::out);
+  }
+  std::ostream& buffer = FLAGS_result.empty() ? std::cout : result;
 
-  std::ifstream wav_scp(FLAGS_wav_scp);
-  std::string line;
-  unsigned int total_waves_dur = 0;
-  unsigned int total_decode_time = 0;
-  while (getline(wav_scp, line)) {
-    std::vector<std::string> strs;
-    wenet::SplitString(line, &strs);
-    CHECK_GE(strs.size(), 2);
-    std::string utt = strs[0];
-    std::string wav = strs[1];
-
-    wenet::WavReader wav_reader(wav);
-    const int sample_rate = 16000;
+  int total_waves_dur = 0;
+  int total_decode_time = 0;
+  for (auto &wav : waves) {
+    wenet::WavReader wav_reader(wav.second);
     CHECK_EQ(wav_reader.sample_rate(), sample_rate);
 
-    auto feature_pipeline =
-        std::make_shared<wenet::FeaturePipeline>(feature_config);
     feature_pipeline->AcceptWaveform(std::vector<float>(
         wav_reader.data(), wav_reader.data() + wav_reader.num_sample()));
     feature_pipeline->set_input_finished();
@@ -65,8 +79,8 @@ int main(int argc, char *argv[]) {
     wenet::TorchAsrDecoder decoder(feature_pipeline, model, symbol_table,
                                    decode_config);
 
-    unsigned int wave_dur = wav_reader.num_sample() / sample_rate * 1000;
-    unsigned int decode_time = 0;
+    int wave_dur = wav_reader.num_sample() / sample_rate * 1000;
+    int decode_time = 0;
     while (true) {
       auto start = std::chrono::steady_clock::now();
       bool finish = decoder.Decode();
@@ -95,7 +109,7 @@ int main(int argc, char *argv[]) {
     LOG(INFO) << "Final result: " << decoder.result();
     LOG(INFO) << "Decoded " << wave_dur << "ms audio taken " << decode_time
               << "ms.";
-    result << utt << " " << decoder.result() << std::endl;
+    buffer << wav.first << " " << decoder.result() << std::endl;
 
     total_waves_dur += wave_dur;
     total_decode_time += decode_time;
