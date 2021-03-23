314880f2500c7e6a580163e6b8b66f3c042f2789
Binbin Zhang <811364747@qq.com>
2021-03-23 22:48:42 +0800

    viterbi score works
    Change-Id: Ie93c5df7c77e6b8b5e3b628deea4be79ba0b4551

--- runtime/core/decoder/ctc_prefix_beam_search_test.cc
+++ runtime/core/decoder/ctc_prefix_beam_search_test.cc
@@ -6,11 +6,13 @@
 #include <cmath>
 #include <vector>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 #include "utils/utils.h"
 
 TEST(CtcPrefixBeamSearchTest, CtcPrefixBeamSearchLogicTest) {
+  using ::testing::ElementsAre;
   // See https://robin1001.github.io/2020/12/11/ctc-search/ for the
   // graph demonstration of the data
   std::vector<float> data = {0.25, 0.40, 0.35, 0.40, 0.35,
@@ -24,22 +26,29 @@ TEST(CtcPrefixBeamSearchTest, CtcPrefixBeamSearchLogicTest) {
   option.second_beam_size = 3;
   wenet::CtcPrefixBeamSearch prefix_beam_search(option);
   prefix_beam_search.Search(input);
-  // top 1: [2, 1] 0.2185
-  // top 2: [1, 2] 0.1550
-  // top 3: [1] 0.1525
+  /* Test case info
+  | top k | result index | prefix score | viterbi score | timestamp |
+  |-------|--------------|--------------|---------------|-----------|
+  | top 1 | [2, 1]       | 0.2185       | 0.07          | [0, 2]    |
+  | top 2 | [1, 2]       | 0.1550       | 0.064         | [0, 2]    |
+  | top 3 | [1]          | 0.1525       | 0.07          | [2]       |
+  */
   const std::vector<std::vector<int>>& result = prefix_beam_search.hypotheses();
-  const std::vector<float>& likelihood = prefix_beam_search.likelihood();
   EXPECT_EQ(result.size(), 3);
+  ASSERT_THAT(result[0], ElementsAre(2, 1));
+  ASSERT_THAT(result[1], ElementsAre(1, 2));
+  ASSERT_THAT(result[2], ElementsAre(1));
+
+  const std::vector<float>& likelihood = prefix_beam_search.likelihood();
   EXPECT_EQ(likelihood.size(), 3);
-  EXPECT_EQ(result[0].size(), 2);
-  EXPECT_EQ(result[0][0], 2);
-  EXPECT_EQ(result[0][1], 1);
-  EXPECT_EQ(result[1].size(), 2);
-  EXPECT_EQ(result[1][0], 1);
-  EXPECT_EQ(result[1][1], 2);
-  EXPECT_EQ(result[2].size(), 1);
-  EXPECT_EQ(result[2][0], 1);
   EXPECT_FLOAT_EQ(std::exp(likelihood[0]), 0.2185);
   EXPECT_FLOAT_EQ(std::exp(likelihood[1]), 0.1550);
   EXPECT_FLOAT_EQ(std::exp(likelihood[2]), 0.1525);
+
+  const std::vector<float>& viterbi_likelihood =
+      prefix_beam_search.viterbi_likelihood();
+  EXPECT_EQ(viterbi_likelihood.size(), 3);
+  EXPECT_FLOAT_EQ(std::exp(viterbi_likelihood[0]), 0.07);
+  EXPECT_FLOAT_EQ(std::exp(viterbi_likelihood[1]), 0.064);
+  EXPECT_FLOAT_EQ(std::exp(viterbi_likelihood[2]), 0.07);
 }
