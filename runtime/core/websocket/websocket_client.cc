47b14e0d1da7df4eb1da12c80375850507440a1e
彭震东 <275331498@qq.com>
2021-03-29 16:25:52 +0800

    [runtime] support nbest and timestamp in client/server protocal (#250)
    * [runtime] add decode result struct
    
    * [runtime] add nbest param in client

--- runtime/core/websocket/websocket_client.cc
+++ runtime/core/websocket/websocket_client.cc
@@ -26,8 +26,8 @@ namespace asio = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;        // from <boost/asio/ip/tcp.hpp>
 namespace json = boost::json;
 
-WebSocketClient::WebSocketClient(const std::string& host, int port)
-    : host_(host), port_(port) {
+WebSocketClient::WebSocketClient(const std::string& host, int port, int nbest)
+    : host_(host), port_(port), nbest_(nbest) {
   Connect();
   t_.reset(new std::thread(&WebSocketClient::ReadLoopFunc, this));
 }
@@ -88,7 +88,7 @@ void WebSocketClient::Join() { t_->join(); }
 
 void WebSocketClient::SendStartSignal() {
   // TODO(Binbin Zhang): Add sample rate and other setting surpport
-  json::value start_tag = {{"signal", "start"}};
+  json::value start_tag = {{"signal", "start"}, {"nbest", nbest_}};
   std::string start_message = json::serialize(start_tag);
   this->SendTextData(start_message);
 }
