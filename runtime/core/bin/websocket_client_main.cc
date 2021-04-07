c1957e96c0e756d221724a3f4b3892a1c42cabf9
Binbin Zhang <811364747@qq.com>
2021-04-07 12:08:03 +0800

    refine include order
    Change-Id: I5571340996f8b67539273256f1890d70cc77759a

--- runtime/core/bin/websocket_client_main.cc
+++ runtime/core/bin/websocket_client_main.cc
@@ -15,8 +15,8 @@
 #include <chrono>
 
 #include "frontend/wav.h"
-#include "utils/log.h"
 #include "utils/flags.h"
+#include "utils/log.h"
 #include "websocket/websocket_client.h"
 
 DEFINE_string(host, "127.0.0.1", "host of websocket server");
