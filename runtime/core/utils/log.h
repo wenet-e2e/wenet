41e0babddae942196126db93b6fe76e7389bba13
pengzhendong <275331498@qq.com>
2021-04-11 10:24:11 +0800

    [runtime] add comments to flags/log

--- runtime/core/utils/log.h
+++ runtime/core/utils/log.h
@@ -15,6 +15,9 @@
 #ifndef UTILS_LOG_H_
 #define UTILS_LOG_H_
 
+// Because openfst is a dynamic library compiled with gflags/glog, we must use
+// the gflags/glog from openfst to avoid them linked both statically and
+// dynamically into the executable.
 #include "fst/log.h"
 
 #endif  // UTILS_LOG_H_
