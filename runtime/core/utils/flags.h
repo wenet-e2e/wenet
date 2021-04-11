e86e5c15f44137efb8abdf4496238771ddcb4aaa
彭震东 <275331498@qq.com>
2021-04-11 17:20:35 +0800

    [runtime] fix bug of some dependencies (#301)
    * [runtime] fix bug of some dependencies
    
    * [runtime] add comments to flags/log
    
    Co-authored-by: randypeng <pengzhendong@Randys-MacBook-Pro.local>

--- runtime/core/utils/flags.h
+++ runtime/core/utils/flags.h
@@ -15,6 +15,9 @@
 #ifndef UTILS_FLAGS_H_
 #define UTILS_FLAGS_H_
 
-#include "gflags/gflags.h"
+// Because openfst is a dynamic library compiled with gflags/glog, we must use
+// the gflags/glog from openfst to avoid them linked both statically and
+// dynamically into the executable.
+#include "fst/flags.h"
 
 #endif  // UTILS_FLAGS_H_
