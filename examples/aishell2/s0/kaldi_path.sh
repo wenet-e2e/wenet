38f2ef0dbb2352baeaa92388ed5f008773bea978
Binbin Zhang <811364747@qq.com>
2021-04-28 10:26:55 +0800

    [examples] add aishell2 lm support (#336)
    * [examples] add aishell2 lm support
    
    Change-Id: I4418f342ea3b4f0b9565b72280b6a8acb27c9647
    
    * remove decode.sh
    
    Change-Id: Ie032ca9097532b9388bb5d21c9da162cb5985f67
    
    * add todo
    
    Change-Id: I189695bc21edd7d7c7f8f8b266deb36453b84e69
    
    * update readme
    
    Change-Id: I530230a35d0c2c9e83ad066de6ecc275203d75b1
    
    * add mkdir -p data/local/dict
    
    Change-Id: I31fab104ee0f22cc6033e11d3d287f40a1a0dd65

---
+++ examples/aishell2/s0/kaldi_path.sh
@@ -0,0 +1,6 @@
+# TODO(Binbin): Will remove this dependency and file in the future
+export KALDI_ROOT=/export/tools/kaldi-tools/
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
--- examples/aishell2/s0/path.sh
+++ examples/aishell2/s0/path.sh
@@ -1,4 +1,4 @@
-export PATH=$PWD:$PATH
+export PATH=$PWD:../../../runtime/server/x86/build/:$PATH
 
 # NOTE(kan-bayashi): Use UTF-8 in Python to avoid UnicodeDecodeError when LC_ALL=C
 export PYTHONIOENCODING=UTF-8
