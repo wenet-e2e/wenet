829fc01b5f4b70a95364fb317d7309387693d0b8
Binbin Zhang <811364747@qq.com>
2021-05-06 11:20:31 +0800

    arpa2fst compile okay

--- runtime/core/kaldi/base/kaldi-error.h
+++ runtime/core/kaldi/base/kaldi-error.h
@@ -31,6 +31,26 @@ namespace kaldi {
 
 #define KALDI_ASSERT(condition) CHECK(condition)
 
+
+/***** PROGRAM NAME AND VERBOSITY LEVEL *****/
+
+/// Called by ParseOptions to set base name (no directory) of the executing
+/// program. The name is printed in logging code along with every message,
+/// because in our scripts, we often mix together the stderr of many programs.
+/// This function is very thread-unsafe.
+void SetProgramName(const char *basename);
+
+/// This is set by util/parse-options.{h,cc} if you set --verbose=? option.
+/// Do not use directly, prefer {Get,Set}VerboseLevel().
+extern int32 g_kaldi_verbose_level;
+
+/// Get verbosity level, usually set via command line '--verbose=' switch.
+inline int32 GetVerboseLevel() { return g_kaldi_verbose_level; }
+
+/// This should be rarely used, except by programs using Kaldi as library;
+/// command-line programs set the verbose level automatically from ParseOptions.
+inline void SetVerboseLevel(int32 i) { g_kaldi_verbose_level = i; }
+
 }  // namespace kaldi
 
 #endif  // KALDI_BASE_KALDI_ERROR_H_
