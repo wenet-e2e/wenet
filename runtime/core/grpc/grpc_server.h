6c4a65950436a121fcebdede209928bd4b2b9059
Xiang Lyu <31685265+aluminumbox@users.noreply.github.com>
2021-05-21 19:42:54 +0800

    fix-ximalaya-corp-name (#397)
    Co-authored-by: xiang.lv <xiang.lv@ximalaya.com>

--- runtime/core/grpc/grpc_server.h
+++ runtime/core/grpc/grpc_server.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Ximalaya Inc (Xiang Lyu)
+// Copyright (c) 2021 Ximalaya Speech Team (Xiang Lyu)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
