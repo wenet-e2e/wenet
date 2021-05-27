cfbfd4275728e69d2cceb41afa46bdd847bfcdd4
Kuang Ru <rukuang10@gmail.com>
2021-05-27 09:59:17 +0800

    [tools] use shebang to specify the interpreter version (#350)

--- tools/wav_to_duration.sh
+++ tools/wav_to_duration.sh
@@ -20,7 +20,7 @@ split --additional-suffix .slice -d -n l/$nj $inscp $logdir/wav_
 for slice in `ls $logdir/wav_*.slice`; do
 {
     name=`basename -s .slice $slice`
-    python tools/wav2dur.py $slice $logdir/$name.shape 1>$logdir/$name.log
+    tools/wav2dur.py $slice $logdir/$name.shape 1>$logdir/$name.log
 } &
 done
 wait
