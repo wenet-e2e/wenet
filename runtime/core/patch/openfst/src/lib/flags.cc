5436f63cef0a35ba92b6bfb89d21f3b8a6325745
pengzhendong <275331498@qq.com>
2021-06-28 14:52:34 +0800

    [runtime] add some comments

--- runtime/core/patch/openfst/src/lib/flags.cc
+++ runtime/core/patch/openfst/src/lib/flags.cc
@@ -12,6 +12,13 @@
 //
 // Google-style flag handling definitions.
 
+#include <cstring>
+
+#if _MSC_VER
+#include <io.h>
+#include <fcntl.h>
+#endif
+
 #include <fst/compat.h>
 #include <fst/flags.h>
 
@@ -20,23 +27,49 @@ static const char *private_tmpdir = getenv("TMPDIR");
 // DEFINE_int32(v, 0, "verbosity level");
 // DEFINE_bool(help, false, "show usage information");
 // DEFINE_bool(helpshort, false, "show brief usage information");
- #ifndef _MSC_VER
- DEFINE_string(tmpdir, private_tmpdir ? private_tmpdir : "/tmp",
-               "temporary directory");
- #else
- DEFINE_string(tmpdir, private_tmpdir ? private_tmpdir : getenv("TEMP"),
-               "temporary directory");
- #endif // !_MSC_VER
+#ifndef _MSC_VER
+DEFINE_string(tmpdir, private_tmpdir ? private_tmpdir : "/tmp",
+              "temporary directory");
+#else
+DEFINE_string(tmpdir, private_tmpdir ? private_tmpdir : getenv("TEMP"),
+              "temporary directory");
+#endif // !_MSC_VER
 
 using namespace std;
 
 static string flag_usage;
 static string prog_src;
 
+// Sets prog_src to src.
+static void SetProgSrc(const char *src) {
+  prog_src = src;
+#if _MSC_VER
+  // This common code is invoked by all FST binaries, and only by them. Switch
+  // stdin and stdout into "binary" mode, so that 0x0A won't be translated into
+  // a 0x0D 0x0A byte pair in a pipe or a shell redirect. Other streams are
+  // already using ios::binary where binary files are read or written.
+  // Kudos to @daanzu for the suggested fix.
+  //    https://github.com/kkm000/openfst/issues/20
+  //    https://github.com/kkm000/openfst/pull/23
+  //    https://github.com/kkm000/openfst/pull/32
+  _setmode(_fileno(stdin), O_BINARY);
+  _setmode(_fileno(stdout), O_BINARY);
+#endif
+  // Remove "-main" in src filename. Flags are defined in fstx.cc but SetFlags()
+  // is called in fstx-main.cc, which results in a filename mismatch in
+  // ShowUsageRestrict() below.
+  static constexpr char kMainSuffix[] = "-main.cc";
+  const int prefix_length = prog_src.size() - strlen(kMainSuffix);
+  if (prefix_length > 0 && prog_src.substr(prefix_length) == kMainSuffix) {
+    prog_src.erase(prefix_length, strlen("-main"));
+  }
+}
+
 void SetFlags(const char *usage, int *argc, char ***argv,
               bool remove_flags, const char *src) {
   flag_usage = usage;
-  prog_src = src;
+  SetProgSrc(src);
+
   int index = 1;
   for (; index < *argc; ++index) {
     string argval = (*argv)[index];
@@ -87,7 +120,7 @@ void SetFlags(const char *usage, int *argc, char ***argv,
 // defined in file 'src' and 'in_src' is false, then print usage.
 static void
 ShowUsageRestrict(const std::set<pair<string, string>> &usage_set,
-      const string &src, bool in_src, bool show_file) {
+                  const string &src, bool in_src, bool show_file) {
   string old_file;
   bool file_out = false;
   bool usage_out = false;
@@ -99,7 +132,7 @@ ShowUsageRestrict(const std::set<pair<string, string>> &usage_set,
     if (file != old_file) {
       if (show_file) {
         if (file_out) cout << "\n";
-      cout << "Flags from: " << file << "\n";
+        cout << "Flags from: " << file << "\n";
         file_out = true;
       }
       old_file = file;
