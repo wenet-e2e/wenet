22c10ca9286ccddb6d7b5d145f6d0b5d3c8ed00d
xingchensong <xingchensong1996@163.com>
2021-07-06 21:35:46 +0800

    [runtime] fix lint

--- runtime/core/backend/inverse_text_normalizer.cc
+++ runtime/core/backend/inverse_text_normalizer.cc
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "inverse_text_normalizer.h"
+#include "backend/inverse_text_normalizer.h"
 
 #include <set>
 
