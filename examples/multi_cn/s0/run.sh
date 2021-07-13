8cf334db298fd5a2d7f7a84c498c1ffe214ab79d
Meng Wei <wmeng94@gmail.com>
2021-07-14 02:03:08 +0800

    update multi_cn clean text

--- examples/multi_cn/s0/run.sh
+++ examples/multi_cn/s0/run.sh
@@ -136,16 +136,16 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     for x in train dev; do
         cp ${feat_dir}_${en_modeling_unit}/${x}/text ${feat_dir}_${en_modeling_unit}/${x}/text.org
         paste -d " " <(cut -f 1 -d" " ${feat_dir}_${en_modeling_unit}/${x}/text.org) <(cut -f 2- -d" " ${feat_dir}_${en_modeling_unit}/${x}/text.org \
-            | tr 'a-z' 'A-Z' | sed 's/\([A-Z]\) \([A-Z]\)/\1▁\2/g' | tr -d " " ) \
+            | tr 'a-z' 'A-Z' | sed 's/\([A-Z]\) \([A-Z]\)/\1▁\2/g' | sed 's/\([A-Z]\) \([A-Z]\)/\1▁\2/g' | tr -d " ") \
             > ${feat_dir}_${en_modeling_unit}/${x}/text
-    sed -i 's/\xEF\xBB\xBF//' ${feat_dir}_${en_modeling_unit}/${x}/text
+        sed -i 's/\xEF\xBB\xBF//' ${feat_dir}_${en_modeling_unit}/${x}/text
 
     done
 
     for x in ${test_sets}; do
         cp ${feat_dir}_${en_modeling_unit}/test_${x}/text ${feat_dir}_${en_modeling_unit}/test_${x}/text.org
         paste -d " " <(cut -f 1 -d" " ${feat_dir}_${en_modeling_unit}/test_${x}/text.org) <(cut -f 2- -d" " ${feat_dir}_${en_modeling_unit}/test_${x}/text.org \
-        | sed 's/\([A-Z]\) \([A-Z]\)/\1▁\2/g' | tr -d " " |  tr 'a-z' 'A-Z') \
+            | tr 'a-z' 'A-Z' | sed 's/\([A-Z]\) \([A-Z]\)/\1▁\2/g' | sed 's/\([A-Z]\) \([A-Z]\)/\1▁\2/g' | tr -d " ") \
             > ${feat_dir}_${en_modeling_unit}/test_${x}/text
         sed -i 's/\xEF\xBB\xBF//' ${feat_dir}_${en_modeling_unit}/test_${x}/text
     done
