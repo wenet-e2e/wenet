2b8521088428733550217ac06268a3a9db1ce571
Binbin Zhang <811364747@qq.com>
2021-07-21 15:29:48 +0800

    [runtime] refactor postprocessing function
    Change-Id: I19782e2ee5d303bcda78306d44756d773ee5c9ff

--- runtime/core/decoder/torch_asr_decoder.cc
+++ runtime/core/decoder/torch_asr_decoder.cc
@@ -36,7 +36,10 @@ TorchAsrDecoder::TorchAsrDecoder(
     searcher_.reset(new CtcWfstBeamSearch(*fst_, opts.ctc_wfst_search_opts));
   }
   ctc_endpointer_->frame_shift_in_ms(frame_shift_in_ms());
+  InitPostProcessing();
+}
 
+void TorchAsrDecoder::InitPostProcessing() {
   fst::SymbolTableIterator iter(*symbol_table_);
   std::string space_symbol = kSpaceSymbol;
   while (!iter.Done()) {
