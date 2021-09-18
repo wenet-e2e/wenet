4a2654502459ee714325e67003762071e607a1a5
Binbin Zhang <811364747@qq.com>
2021-09-18 04:18:20 +0000

    add shard tools and example on aishell
    Change-Id: Ieab9f6287f34da694cf8ac26a66c15254cb6bf2c

---
+++ tools/make_shard_list.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2021 Mobvoi Inc. (authors: Binbin Zhang)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import io
+import os
+import sys
+import tarfile
+from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
+
+AUDIO_FORMAT_SETS = set(['flac', 'mp3', 'm4a', 'ogg', 'opus', 'wav', 'wma'])
+
+
+def write_tar_file(data_list, tar_file):
+    print('Processing {}'.format(tar_file))
+    with tarfile.open(tar_file, "w:gz") as tar:
+        for key, wav, txt in data_list:
+            suffix = wav.split('.')[-1]
+            assert suffix in AUDIO_FORMAT_SETS
+            wav_file = key + '.' + suffix
+            txt_file = key + '.txt'
+            with open(wav, 'rb') as fin:
+                data = fin.read()
+            wav_data = io.BytesIO(data)
+            wav_info = tarfile.TarInfo(wav_file)
+            wav_info.size = len(data)
+
+            assert isinstance(txt, str)
+            txt = txt.encode('utf8')
+            txt_data = io.BytesIO(txt)
+            txt_info = tarfile.TarInfo(txt_file)
+            txt_info.size = len(txt)
+
+            tar.addfile(wav_info, wav_data)
+            tar.addfile(txt_info, txt_data)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--num_utts_per_shard',
+                        type=int,
+                        default=1000,
+                        help='num utts per shard')
+    parser.add_argument('--num_threads',
+                        type=int,
+                        default=1,
+                        help='num threads for make shards')
+    parser.add_argument('--prefix',
+                        default='shards',
+                        help='prefix of shards tar file')
+    parser.add_argument('wav_file', help='wav file')
+    parser.add_argument('text_file', help='text file')
+    parser.add_argument('shards_dir', help='output shards dir')
+    parser.add_argument('shards_list', help='output shards list file')
+    args = parser.parse_args()
+
+    wav_table = {}
+    with open(args.wav_file, 'r', encoding='utf8') as fin:
+        for line in fin:
+            arr = line.strip().split()
+            assert len(arr) == 2
+            wav_table[arr[0]] = arr[1]
+
+    data = []
+    with open(args.text_file, 'r', encoding='utf8') as fin:
+        for line in fin:
+            arr = line.strip().split(maxsplit=1)
+            key = arr[0]
+            txt = arr[1]
+            assert key in wav_table
+            wav = wav_table[key]
+            data.append((key, wav, txt))
+
+    num = args.num_utts_per_shard
+    chunks = [data[i:i + num] for i in range(0, len(data), num)]
+    os.makedirs(args.shards_dir, exist_ok=True)
+
+    # Using thread pool to speedup
+    pool = ThreadPoolExecutor(args.num_threads)
+    shards_list = []
+    tasks_list = []
+    for i, chunk in enumerate(chunks):
+        tar_file = os.path.join(args.shards_dir,
+                                '{}_{:09d}.tar.gz'.format(args.prefix, i))
+        shards_list.append(tar_file)
+        sys.stdout.flush()
+        task = pool.submit(write_tar_file, chunk, tar_file)
+
+    wait(tasks_list, return_when=ALL_COMPLETED)
+
+    with open(args.shards_list, 'w', encoding='utf8') as fout:
+        for name in shards_list:
+            fout.write(name + '\n')
