7b95d524ce7db469177e162125c78eebb4b9eab4
Binbin Zhang <811364747@qq.com>
2021-10-19 09:54:28 +0000

    [dataset] support segments in raw io
    Change-Id: Ifa6b5c9fc09c8eae3f220b32e674c3d660d2977c

--- tools/make_raw_list.py
+++ tools/make_raw_list.py
@@ -14,29 +14,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
+import argparse
 import json
 
-wav_file = sys.argv[1]
-text_file = sys.argv[2]
-output_file = sys.argv[3]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--segments', default=None, help='segments file')
+    parser.add_argument('wav_file', help='wav file')
+    parser.add_argument('text_file', help='text file')
+    parser.add_argument('output_file', help='output list file')
+    args = parser.parse_args()
 
-wav_table = {}
-with open(wav_file, 'r', encoding='utf8') as fin:
-    for line in fin:
-        arr = line.strip().split()
-        assert len(arr) == 2
-        wav_table[arr[0]] = arr[1]
+    wav_table = {}
+    with open(args.wav_file, 'r', encoding='utf8') as fin:
+        for line in fin:
+            arr = line.strip().split()
+            assert len(arr) == 2
+            wav_table[arr[0]] = arr[1]
 
+    if args.segments is not None:
+        segments_table = {}
+        with open(args.segments, 'r', encoding='utf8') as fin:
+            for line in fin:
+                arr = line.strip().split()
+                assert len(arr) == 4
+                segments_table[arr[0]] = (arr[1], float(arr[2]), float(arr[3]))
 
-with open(text_file, 'r', encoding='utf8') as fin, \
-     open(output_file, 'w', encoding='utf8') as fout:
-    for line in fin:
-        arr = line.strip().split(maxsplit=1)
-        key = arr[0]
-        txt = arr[1] if len(arr) > 1 else ''
-        assert key in wav_table
-        wav = wav_table[key]
-        line = dict(key=key, wav=wav, txt=txt)
-        json_line = json.dumps(line, ensure_ascii=False)
-        fout.write(json_line + '\n')
+    with open(args.text_file, 'r', encoding='utf8') as fin, \
+         open(args.output_file, 'w', encoding='utf8') as fout:
+        for line in fin:
+            arr = line.strip().split(maxsplit=1)
+            key = arr[0]
+            txt = arr[1] if len(arr) > 1 else ''
+            if args.segments is None:
+                assert key in wav_table
+                wav = wav_table[key]
+                line = dict(key=key, wav=wav, txt=txt)
+            else:
+                assert key in segments_table
+                wav_key, start, end = segments_table[key]
+                wav = wav_table[wav_key]
+                line = dict(key=key, wav=wav, txt=txt, start=start, end=end)
+            json_line = json.dumps(line, ensure_ascii=False)
+            fout.write(json_line + '\n')
