cafb956e0900da8192883edef81fa941c2ea9ac5
Yougen Yuan <yougenyuan@gmail.com>
2021-10-26 20:53:17 +0800

    [examples] format space in vkw2021 (#731)
    * format space
    
    * format space
    
    * format space
    
    Co-authored-by: yougenyuan <yougenyuan@tencent.com>

--- examples/vkw2021/s0/local/vkw_data_prep.sh
+++ examples/vkw2021/s0/local/vkw_data_prep.sh
@@ -8,15 +8,15 @@ stop_stage=0
 . ./path.sh || exit 1;
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
-    cd $current_dir/data/
-    [ ! -z vkw_v1.1.zip ] && echo "wget vkw challenge data to this directory" && exit 0
-    [ ! -z vkw ] && unzip vkw_v1.1.zip
-    cd $current_dir
+  cd $current_dir/data/
+  [ ! -z vkw_v1.1.zip ] && echo "wget vkw challenge data to this directory" && exit 0
+  [ ! -z vkw ] && unzip vkw_v1.1.zip
+  cd $current_dir
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-   x=train
-   [ ! -f data/${x}/text ] && echo "vkw trainset is missing, wget to this directory" && exit 0
+  x=train
+  [ ! -f data/${x}/text ] && echo "vkw trainset is missing, wget to this directory" && exit 0
 fi
 
 echo "$0: vkw  data preparation succeeded"
