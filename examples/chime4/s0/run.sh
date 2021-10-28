098ca052ef936855b5e4e86fbd19f3aa0643523f
funcwj <funcwj@foxmail.com>
2021-10-28 11:54:18 +0000

    update decoding recipe in run,sh

--- examples/chime4/s0/run.sh
+++ examples/chime4/s0/run.sh
@@ -17,6 +17,8 @@ dict=$data_dir/dict_char.txt
 train_config=conf/train_conformer.yaml
 exp_dir=exp/1a
 decode_modes="ctc_prefix_beam_search attention"
+average_checkpoint=true
+average_num=10
 
 . ./path.sh
 . ./tools/parse_options.sh || exit 1
@@ -38,12 +40,12 @@ fi
 
 
 if [ $end -ge 2 ] && [ $beg -le 2 ]; then
-  echo "<NOISE>" > $data_dir/train/non_lang.txt
+  echo -e "<NOISE>\n<*IN*>\n<*MR.*>" > $data_dir/train/non_lang.txt
   for name in dev train; do
     python tools/text2token.py $data_dir/$name/text -n 1 -s 1 \
       -l $data_dir/train/non_lang.txt > $data_dir/$name/char
   done
-  mkdir -p $(dirname $dict) && echo -e "<blank> 0\n<unk> 1" >> ${dict}
+  mkdir -p $(dirname $dict) && echo -e "<blank> 0\n<unk> 1" > ${dict}
   cat $data_dir/train/char | cut -f 2- -d" " | tr " " "\n" | sort | uniq | awk '{print $0 " " NR+1}' >> ${dict}
   num_token=$(cat $dict | wc -l)
   echo "<sos/eos> $num_token" >> $dict
@@ -59,13 +61,13 @@ if [ $end -ge 3 ] && [ $beg -le 3 ]; then
     --out_cmvn $data_dir/train/global_cmvn
   echo "Prepare data, prepare requried format"
   for x in train dev; do
-  tools/make_raw_list.py $data_dir/$x/wav.scp $data_dir/$x/text \
+  tools/make_raw_list.py $data_dir/$x/wav.scp $data_dir/$x/char \
     $data_dir/$x/data.list
   done
 fi
 
 if [ $end -ge 4 ] && [ $beg -le 4 ]; then
-  mkdir -p $exp_dir && cp feat/train/global_cmvn $exp_dir
+  mkdir -p $exp_dir && cp $data_dir/train/global_cmvn $exp_dir
   python wenet/bin/train.py \
     --gpu 0 \
     --config $train_config \
@@ -80,8 +82,17 @@ fi
 
 suffix="isolated_1ch_track"
 if [ $end -ge 5 ] && [ $beg -le 5 ]; then
+  if [ ${average_checkpoint} == true ]; then
+    decode_checkpoint=$exp_dir/avg_${average_num}.pt
+    echo "do model average and final checkpoint is $decode_checkpoint"
+    python wenet/bin/average_model.py \
+      --dst_model $decode_checkpoint \
+      --src_path $exp_dir  \
+      --num ${average_num} \
+      --val_best
+  fi
   nj=4
-  ctc_weight=0.5
+  ctc_weight=0.2
   for x in dt05_{simu,real} et05_{simu,real}; do
     subdir=${x}_${suffix}
     tools/make_raw_list.py $data_dir/$subdir/wav.scp $data_dir/$subdir/text \
@@ -89,27 +100,29 @@ if [ $end -ge 5 ] && [ $beg -le 5 ]; then
   done
   for mode in ${decode_modes}; do
     for x in dt05_{simu,real} et05_{simu,real}; do
+      subdir=${x}_${suffix}
       dec_dir=$exp_dir/${subdir}_${mode} && mkdir -p $dec_dir
       python wenet/bin/recognize.py \
         --gpu 0 \
   	--mode $mode \
         --config $exp_dir/train.yaml \
   	--test_data $data_dir/$subdir/data.list \
-  	--checkpoint $exp_dir/70.pt \
+  	--checkpoint $exp_dir/avg_${average_num}.pt \
   	--beam_size 8 \
         --batch_size 1 \
         --dict $dict \
- 	--ctc_weight $ctc_weight \
+  	--ctc_weight $ctc_weight \
    	--result_file $dec_dir/text &
      done
      wait
   done
   for mode in ${decode_modes}; do
     for x in dt05_{simu,real} et05_{simu,real}; do
+     subdir=${x}_${suffix}
      dec_dir=$exp_dir/${subdir}_${mode}
      sed 's:<space>: :g' $dec_dir/text > $dec_dir/text.norm
      python tools/compute-wer.py --char=1 --v=1 \
-       feat/$subdir/text $dec_dir/text.norm > $dec_dir/wer
+       $data_dir/$subdir/text $dec_dir/text.norm > $dec_dir/wer
     done
   done
 fi
