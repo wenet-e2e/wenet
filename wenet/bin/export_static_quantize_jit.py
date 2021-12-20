5681887e338e4c8b2c75ffc283140e11a9d56a6d
Lucky Wong <lekai.huang@gmail.com>
2021-12-20 17:22:26 +0800

    Add author name.

--- wenet/bin/export_static_quantize_jit.py
+++ wenet/bin/export_static_quantize_jit.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Binbin Zhang(binbzha@qq.com)
+# Copyright (c) 2021 Binbin Zhang(binbzha@qq.com), Lucky Wong
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
