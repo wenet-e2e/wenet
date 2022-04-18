8a89ec840e41dbe4fac2ba739eb2ab7a6fffb35a
Tong Jinle <tongjinle123@live.com>
2022-04-18 16:32:01 +0800

    [network] rmake lable_smoothing_loss and add_sos_eos run on IPU

--- wenet/transformer/label_smoothing_loss.py
+++ wenet/transformer/label_smoothing_loss.py
@@ -48,11 +48,11 @@ class LabelSmoothingLoss(nn.Module):
                  normalize_length: bool = False):
         """Construct an LabelSmoothingLoss object."""
         super(LabelSmoothingLoss, self).__init__()
-        self.criterion = nn.KLDivLoss(reduction="none")
         self.padding_idx = padding_idx
-        self.confidence = 1.0 - smoothing
         self.smoothing = smoothing
-        self.size = size
+        self.confidence = 1.0 - smoothing
+        self.other_value = self.smoothing / (self.size - 1)
+        self.offset = self.confidence - self.other_value
         self.normalize_length = normalize_length
 
     def forward(self, x: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
@@ -69,18 +69,16 @@ class LabelSmoothingLoss(nn.Module):
         Returns:
             loss (torch.Tensor) : The KL loss, scalar float value
         """
-        assert x.size(2) == self.size
-        batch_size = x.size(0)
-        x = x.view(-1, self.size)
-        target = target.view(-1)
-        # use zeros_like instead of torch.no_grad() for true_dist,
-        # since no_grad() can not be exported by JIT
-        true_dist = torch.zeros_like(x)
-        true_dist.fill_(self.smoothing / (self.size - 1))
-        ignore = target == self.padding_idx  # (B,)
-        total = len(target) - ignore.sum().item()
-        target = target.masked_fill(ignore, 0)  # avoid -1 index
-        true_dist.scatter_(1, target.unsqueeze(1), self.confidence)
-        kl = self.criterion(torch.log_softmax(x, dim=1), true_dist)
-        denom = total if self.normalize_length else batch_size
-        return kl.masked_fill(ignore.unsqueeze(1), 0).sum() / denom
+        target_mask = target.ne(self.padding_idx)
+        target = torch.clamp_min(target.float(), 0.0)
+        target = target.long()
+        y_true = torch.nn.functional.one_hot(target, self.size).to(x.dtype)
+        y_true = y_true * self.offset + self.other_value
+        y_pred = torch.log_softmax(x, -1)
+        loss_pre = y_true * (torch.log(y_true) - y_pred) * target_mask.unsqueeze(-1)
+        loss = torch.sum(loss_pre)
+        if self.normalize_length:
+            loss = loss / target_mask.int().sum()
+        else:
+            loss = loss / target.size(0)
+        return loss
