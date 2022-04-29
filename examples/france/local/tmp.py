b27743efc6096cd7adf430d564194cee6da9cb76
wrz1999 <609519258@qq.com>
2022-04-30 02:41:06 +0800

    t2

--- examples/france/local/tmp.py
+++ examples/france/local/tmp.py
@@ -5,8 +5,8 @@ import sys
 import os
 
 def process(src_str):
-   punc = '~`!#$%^&*()_+-=|\';":/.,?><~·！@#￥%……&*（）——+-=“：’；、。，？》《{}'
-   return(re.sub(r"[%s]+" %punc, "",src_str)).upper()
+    punc = '~`!#$%^&*()_+-=|\';":/.,?><~·！@#￥%……&*（）——+-=“：’；、。，？》《{}'
+    return(re.sub(r"[%s]+" %punc, "",src_str)).upper()
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
