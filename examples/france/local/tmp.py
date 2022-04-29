03efb98c0053adce52aa11be777ba531b68a265f
wrz1999 <609519258@qq.com>
2022-04-30 01:59:51 +0800

    debug_19

--- examples/france/local/tmp.py
+++ examples/france/local/tmp.py
@@ -6,21 +6,7 @@ import os
 
 
 def sph2pipe_wav(in_wav, tmp_out_wav, out_wav):
-    with open(in_wav, 'r', encoding='utf-8') as in_f:
-        with open(tmp_out_wav, 'w', encoding='utf-8') as tmp_out_f:
-            with open(out_wav, 'w', encoding='utf-8') as out_f:
-                for line in in_f:
-                    _tmp = line.strip().split(' ')
-                    wav_out_path = _tmp[4]
-                    wav_out_path = wav_out_path.split('/')
-                    wav_out_path[-4] = wav_out_path[-4] + '_pipe'
-                    if not os.path.exists('/'.join(wav_out_path[:-1])):
-                        os.makedirs('/'.join(wav_out_path[:-1]))
-                    wav_out_path = '/'.join(wav_out_path)
-                    tmp_out_f.write(' '.join(_tmp[1:5]) + ' ' + wav_out_path +
-                                    '\n')
-                    out_f.write(_tmp[0] + ' ' + wav_out_path + '\n')
-
+    pass
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -29,9 +15,6 @@ if __name__ == '__main__':
     src_dir = sys.argv[1]
     tsv_file = src_dir + "/" + sys.argv[2] + ".tsv"
     output_dir = sys.argv[3]
-    for file_path in os.listdir(src_dir+"/clips"):
-        if(os.path.exists(src_dir + "/wavs/" + file_path.split('.')[0] + ".wav")):
-            continue
     import pandas
     tsv_content = pandas.read_csv(tsv_file, sep="\t")
     path_list = tsv_content["path"]
