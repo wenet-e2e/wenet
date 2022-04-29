6942932fbfe7789c0bd94fd90879bcce9fc0e67b
wrz1999 <609519258@qq.com>
2022-04-30 00:25:04 +0800

    delete zhushi  tmp.py

--- examples/france/local/tmp.py
+++ examples/france/local/tmp.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import sys
 if __name__ == '__main__':
     print("1")
-    #args = sys.argv
-    #if len(sys.argv) != 3:
-    #    print("wrong input")
-    #    raise NotImplementedError(len(sys.argv))
 
