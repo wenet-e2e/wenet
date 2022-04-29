7996d8575763641229643b4f5c908f54e05fa40c
wrz1999 <609519258@qq.com>
2022-04-30 00:21:13 +0800

    only tmp.py

--- examples/france/local/tmp.py
+++ examples/france/local/tmp.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import sys
 if __name__ == '__main__':
-    args = sys.argv
-    if len(sys.argv) != 3:
-        print("wrong input")
-        raise NotImplementedError(len(sys.argv))
+    print("1")
+    #args = sys.argv
+    #if len(sys.argv) != 3:
+    #    print("wrong input")
+    #    raise NotImplementedError(len(sys.argv))
 
