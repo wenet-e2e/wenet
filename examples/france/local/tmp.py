3064fd14697b015010a90111972d83b94db9e32d
wrz1999 <609519258@qq.com>
2022-04-30 01:00:34 +0800

    debug_7

--- examples/france/local/tmp.py
+++ examples/france/local/tmp.py
@@ -33,3 +33,17 @@ if __name__ == '__main__':
     tsv_content = pandas.read_csv(tsv_file, sep="\t")
     path_list = tsv_content["path"]
     sentence = tsv_content["sentence"]
+    client_list = tsv_content["client_id"]
+    scp_file = open(output_dir + "/wav.scp", "w")
+    text_file = open(output_dir + "/text", "w")
+    utt2spk = open(output_dir + "/utt2spk", "w")
+    for i in range(len(path_list)):
+        wav_file = src_dir + "/wavs/" + \
+                path_list[i].split(".")[0] + ".wav"
+        now_sentence = sentence[i]
+        scp_file.writelines(path_list[i].split(".")[0] + " " + wav_file + "\n")
+        text_file.writelines(path_list[i].split(".")[0] + " " + now_sentence + "\n")
+        utt2spk.writelines(path_list[i].split(".")[0] + " " + client_list[i] + "\n")
+    scp_file.close()
+    text_file.close()
+    utt2spk.close()
