62f18370c3f267549e386f0883101e808aee2f0a
wrz1999 <609519258@qq.com>
2022-04-30 00:14:23 +0800

    add simple tmp.py

---
+++ examples/france/local/tmp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(sys.argv) != 3:
+        print("wrong input")
+        raise NotImplementedError(len(sys.argv))
+
