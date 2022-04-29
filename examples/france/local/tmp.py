6abd0f3c4ec4ca26286c91d1569b48325a4a31e2
wrz1999 <609519258@qq.com>
2022-04-29 23:32:40 +0800

    debug_1

--- examples/france/local/tmp.py
+++ examples/france/local/tmp.py
@@ -16,22 +16,20 @@ for file_path in os.listdir(src_dir+"/clips"):
         continue
     os.system("ffmpeg -i {0} -ac 1 -ar 16000 -f wav {1}".format(src_dir+"/clips/"+file_path, src_dir+"/wavs/"+file_path.split('.')[0]+".wav"))
 
-tsv_content = pd.read_csv(tsv_file, sep="\t")
-print(tsv_content.head(5))
-path_list = tsv_content["path"]
-sentence = tsv_content["sentence"]
-client_list = tsv_content["client_id"]
-print(sentence[0])
-scp_file = open(output_dir+"/wav.scp","w")
-text_file = open(output_dir+"/text","w")
-utt2spk = open(output_dir+"/utt2spk", "w")
-for i in range(len(path_list)):
-    wav_file = src_dir+"/wavs/"+path_list[i].split(".")[0]+".wav"
-    now_sentence = process(sentence[i])
-    scp_file.writelines(path_list[i].split(".")[0]+" "+wav_file+"\n")
-    text_file.writelines(path_list[i].split(".")[0]+" "+now_sentence+"\n")
-    utt2spk.writelines(path_list[i].split(".")[0]+" "+client_list[i]+"\n")
-scp_file.close()
-text_file.close()
-utt2spk.close()
+#tsv_content = pd.read_csv(tsv_file, sep="\t")
+#path_list = tsv_content["path"]
+#sentence = tsv_content["sentence"]
+#client_list = tsv_content["client_id"]
+#scp_file = open(output_dir+"/wav.scp","w")
+#text_file = open(output_dir+"/text","w")
+#utt2spk = open(output_dir+"/utt2spk", "w")
+#for i in range(len(path_list)):
+#    wav_file = src_dir+"/wavs/"+path_list[i].split(".")[0]+".wav"
+#    now_sentence = process(sentence[i])
+#    scp_file.writelines(path_list[i].split(".")[0]+" "+wav_file+"\n")
+#    text_file.writelines(path_list[i].split(".")[0]+" "+now_sentence+"\n")
+#    utt2spk.writelines(path_list[i].split(".")[0]+" "+client_list[i]+"\n")
+#scp_file.close()
+#text_file.close()
+#utt2spk.close()
 
