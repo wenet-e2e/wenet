69ce0c1cab3a6641c2618b0374119a6c98e95363
pengzhendong <275331498@qq.com>
2022-05-21 23:34:27 +0800

    [runtime] patch openfst before configuration

--- runtime/core/cmake/openfst.cmake
+++ runtime/core/cmake/openfst.cmake
@@ -14,10 +14,10 @@ endif()
 # So, we use "OpenFST port for Windows" to build openfst with cmake in Windows.
 # Openfst is compiled with glog/gflags to avoid log and flag conflicts with log and flags in wenet/libtorch.
 # To build openfst with gflags and glog, we comment out some vars of {flags, log}.h and flags.cc.
+set(openfst_SOURCE_DIR ${fc_base}/openfst-src CACHE PATH "OpenFST source directory")
+set(openfst_BINARY_DIR ${fc_base}/openfst-build CACHE PATH "OpenFST build directory")
+set(openfst_PREFIX_DIR ${fc_base}/openfst-subbuild/openfst-populate-prefix CACHE PATH "OpenFST prefix directory")
 if(NOT MSVC)
-  set(openfst_SOURCE_DIR ${fc_base}/openfst-src CACHE PATH "OpenFST source directory")
-  set(openfst_BINARY_DIR ${fc_base}/openfst-build CACHE PATH "OpenFST build directory")
-  set(openfst_PREFIX_DIR ${fc_base}/openfst-subbuild/openfst-populate-prefix CACHE PATH "OpenFST prefix directory")
   ExternalProject_Add(openfst
     URL               https://github.com/mjansche/openfst/archive/1.6.5.zip
     URL_HASH          SHA256=b720357a464f42e181d7e33f60867b54044007f50baedc8f4458a3926f4a5a78
@@ -36,11 +36,11 @@ if(NOT MSVC)
 else()
   add_compile_options(/W0 /wd4244 /wd4267)
   FetchContent_Declare(openfst
-    URL      https://github.com/kkm000/openfst/archive/refs/tags/win/1.6.5.1.tar.gz
-    URL_HASH SHA256=02c49b559c3976a536876063369efc0e41ab374be1035918036474343877046e
+    URL           https://github.com/kkm000/openfst/archive/refs/tags/win/1.6.5.1.tar.gz
+    URL_HASH      SHA256=02c49b559c3976a536876063369efc0e41ab374be1035918036474343877046e
+    PATCH_COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/patch/openfst ${openfst_SOURCE_DIR}
   )
   FetchContent_MakeAvailable(openfst)
-  execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/patch/openfst ${openfst_SOURCE_DIR})
   add_dependencies(fst gflags glog)
   target_link_libraries(fst PUBLIC gflags_nothreads_static glog)
 endif()
