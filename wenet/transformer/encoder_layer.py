8e0c5f767ab2413f1cade780451f4072402094f9
zyziszy <zyziszy@foxmail.com>
2022-05-22 20:30:39 +0800

    apply same refactor in ConformerEncoderLayer

--- wenet/transformer/encoder_layer.py
+++ wenet/transformer/encoder_layer.py
@@ -166,7 +166,8 @@ class ConformerEncoderLayer(nn.Module):
         self.size = size
         self.normalize_before = normalize_before
         self.concat_after = concat_after
-        self.concat_linear = nn.Linear(size + size, size)
+        if self.concat_after:
+            self.concat_linear = nn.Linear(size + size, size)
 
     def forward(
         self,
