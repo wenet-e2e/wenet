ae336affd86377833c9d36f9d12d7f2ee6b7b162
pgzhang <37991273+pgzhang@users.noreply.github.com>
2022-06-08 15:48:57 +0800

    [utils]bugfix: pretrained encoder model path (#1204)

--- wenet/utils/checkpoint.py
+++ wenet/utils/checkpoint.py
@@ -96,7 +96,7 @@ def load_trained_modules(model: torch.nn.Module, args: None):
                 partial_state_dict[key] = value
         main_state_dict.update(partial_state_dict)
     else:
-        logging.warning("model was not found : %s", model_path)
+        logging.warning("model was not found : %s", enc_model_path)
 
     model.load_state_dict(main_state_dict)
     configs = {}
