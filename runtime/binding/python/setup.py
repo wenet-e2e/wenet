8ae1a8e175e6df23fbe93246de8a589b7d874754
Binbin Zhang <binbzha@qq.com>
2022-07-02 22:46:04 +0800

    [binding] support continuous decoding

--- runtime/binding/python/setup.py
+++ runtime/binding/python/setup.py
@@ -84,7 +84,7 @@ package_name = "wenet"
 
 setuptools.setup(
     name=package_name,
-    version='1.0.5',
+    version='1.0.6',
     author="Binbin Zhang",
     author_email="binbzha@qq.com",
     package_dir={
