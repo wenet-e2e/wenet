d196c25dbe7e9a911938d661a507000e2c596642
Nickolay V. Shmyrev <nshmyrev@gmail.com>
2022-08-23 16:27:43 +0300

    Allow zero warmup steps (#1398)

--- wenet/utils/scheduler.py
+++ wenet/utils/scheduler.py
@@ -56,12 +56,18 @@ class WarmupLR(_LRScheduler):
 
     def get_lr(self):
         step_num = self.last_epoch + 1
-        return [
-            lr
-            * self.warmup_steps ** 0.5
-            * min(step_num ** -0.5, step_num * self.warmup_steps ** -1.5)
-            for lr in self.base_lrs
-        ]
+        if self.warmup_steps == 0:
+            return [
+                lr * step_num ** -0.5
+                for lr in self.base_lrs
+            ]
+        else:
+            return [
+                lr
+                * self.warmup_steps ** 0.5
+                * min(step_num ** -0.5, step_num * self.warmup_steps ** -1.5)
+                for lr in self.base_lrs
+            ]
 
     def set_step(self, step: int):
         self.last_epoch = step
