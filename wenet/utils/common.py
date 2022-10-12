94fb11abbb984b29cd2e23bac4771e65a0acd484
xingchensong <xingchensong1996@163.com>
2022-10-12 12:12:21 +0800

    feat(rapid-u2++): Minor fix

--- wenet/utils/common.py
+++ wenet/utils/common.py
@@ -1,7 +1,21 @@
+# Copyright (c) 2020 Mobvoi Inc (Binbin Zhang)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Modified from ESPnet(https://github.com/espnet/espnet)
 """Unility functions for Transformer."""
 
 import math
-from typing import Tuple, List
+from typing import List, Tuple
 
 import torch
 from torch.nn.utils.rnn import pad_sequence
@@ -11,14 +25,11 @@ IGNORE_ID = -1
 
 def pad_list(xs: List[torch.Tensor], pad_value: int):
     """Perform padding for the list of tensors.
-
     Args:
         xs (List): List of Tensors [(T_1, `*`), (T_2, `*`), ..., (T_B, `*`)].
         pad_value (float): Value for padding.
-
     Returns:
         Tensor: Padded tensor (B, Tmax, `*`).
-
     Examples:
         >>> x = [torch.ones(4), torch.ones(2), torch.ones(1)]
         >>> x
@@ -27,7 +38,6 @@ def pad_list(xs: List[torch.Tensor], pad_value: int):
         tensor([[1., 1., 1., 1.],
                 [1., 1., 0., 0.],
                 [1., 0., 0., 0.]])
-
     """
     n_batch = len(xs)
     max_len = max([x.size(0) for x in xs])
@@ -39,20 +49,48 @@ def pad_list(xs: List[torch.Tensor], pad_value: int):
     return pad
 
 
+def add_blank(ys_pad: torch.Tensor, blank: int,
+              ignore_id: int) -> torch.Tensor:
+    """ Prepad blank for transducer predictor
+    Args:
+        ys_pad (torch.Tensor): batch of padded target sequences (B, Lmax)
+        blank (int): index of <blank>
+    Returns:
+        ys_in (torch.Tensor) : (B, Lmax + 1)
+    Examples:
+        >>> blank = 0
+        >>> ignore_id = -1
+        >>> ys_pad
+        tensor([[ 1,  2,  3,   4,   5],
+                [ 4,  5,  6,  -1,  -1],
+                [ 7,  8,  9,  -1,  -1]], dtype=torch.int32)
+        >>> ys_in = add_blank(ys_pad, 0, -1)
+        >>> ys_in
+        tensor([[0,  1,  2,  3,  4,  5],
+                [0,  4,  5,  6,  0,  0],
+                [0,  7,  8,  9,  0,  0]])
+    """
+    bs = ys_pad.size(0)
+    _blank = torch.tensor([blank],
+                          dtype=torch.long,
+                          requires_grad=False,
+                          device=ys_pad.device)
+    _blank = _blank.repeat(bs).unsqueeze(1)  # [bs,1]
+    out = torch.cat([_blank, ys_pad], dim=1)  # [bs, Lmax+1]
+    return torch.where(out == ignore_id, blank, out)
+
+
 def add_sos_eos(ys_pad: torch.Tensor, sos: int, eos: int,
                 ignore_id: int) -> Tuple[torch.Tensor, torch.Tensor]:
     """Add <sos> and <eos> labels.
-
     Args:
         ys_pad (torch.Tensor): batch of padded target sequences (B, Lmax)
         sos (int): index of <sos>
         eos (int): index of <eeos>
         ignore_id (int): index of padding
-
     Returns:
         ys_in (torch.Tensor) : (B, Lmax + 1)
         ys_out (torch.Tensor) : (B, Lmax + 1)
-
     Examples:
         >>> sos_id = 10
         >>> eos_id = 11
@@ -89,15 +127,12 @@ def reverse_pad_list(ys_pad: torch.Tensor,
                      ys_lens: torch.Tensor,
                      pad_value: float = -1.0) -> torch.Tensor:
     """Reverse padding for the list of tensors.
-
     Args:
         ys_pad (tensor): The padded tensor (B, Tokenmax).
         ys_lens (tensor): The lens of token seqs (B)
         pad_value (int): Value for padding.
-
     Returns:
         Tensor: Padded tensor (B, Tokenmax).
-
     Examples:
         >>> x
         tensor([[1, 2, 3, 4], [5, 6, 7, 0], [8, 9, 0, 0]])
@@ -105,7 +140,6 @@ def reverse_pad_list(ys_pad: torch.Tensor,
         tensor([[4, 3, 2, 1],
                 [7, 6, 5, 0],
                 [9, 8, 0, 0]])
-
     """
     r_ys_pad = pad_sequence([(torch.flip(y.int()[:i], [0]))
                              for y, i in zip(ys_pad, ys_lens)], True,
@@ -116,15 +150,12 @@ def reverse_pad_list(ys_pad: torch.Tensor,
 def th_accuracy(pad_outputs: torch.Tensor, pad_targets: torch.Tensor,
                 ignore_label: int) -> float:
     """Calculate accuracy.
-
     Args:
         pad_outputs (Tensor): Prediction tensors (B * Lmax, D).
         pad_targets (LongTensor): Target label tensors (B, Lmax, D).
         ignore_label (int): Ignore label id.
-
     Returns:
         float: Accuracy value (0.0 - 1.0).
-
     """
     pad_pred = pad_outputs.view(pad_targets.size(0), pad_targets.size(1),
                                 pad_outputs.size(1)).argmax(2)
@@ -133,6 +164,16 @@ def th_accuracy(pad_outputs: torch.Tensor, pad_targets: torch.Tensor,
         pad_pred.masked_select(mask) == pad_targets.masked_select(mask))
     denominator = torch.sum(mask)
     return float(numerator) / float(denominator)
+
+
+def get_rnn(rnn_type: str) -> torch.nn.Module:
+    assert rnn_type in ["rnn", "lstm", "gru"]
+    if rnn_type == "rnn":
+        return torch.nn.RNN
+    elif rnn_type == "lstm":
+        return torch.nn.LSTM
+    else:
+        return torch.nn.GRU
 
 
 def get_activation(act):
