bbc7c1580e11088b131491d00b490a2a52defc57
veelion <veelion@gmail.com>
2022-11-03 17:17:34 +0800

    fix flake8 error

--- runtime/binding/python/py/__init__.py
+++ runtime/binding/python/py/__init__.py
@@ -1,3 +1,3 @@
 from .decoder import Decoder  # noqa
-from .batch_decoder import BatchDecoder
+from .batch_decoder import BatchDecoder  # noqa
 from _wenet import wenet_set_log_level as set_log_level  # noqa
