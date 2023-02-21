aa0be36885483651331aea2ad77c495bbde875a3
Zhen Zhao (Fiona) <fiona.zhao@intel.com>
2023-02-21 10:08:39 +0800

    Update ov_asr_model.h
    Add lic claim

--- runtime/core/decoder/ov_asr_model.h
+++ runtime/core/decoder/ov_asr_model.h
@@ -1,3 +1,20 @@
+// Copyright (c) 2020 Mobvoi Inc (Binbin Zhang, Di Wu)
+//               2022 ZeXuan Li (lizexuan@huya.com)
+//                    Xingchen Song(sxc19@mails.tsinghua.edu.cn)
+//                    hamddct@gmail.com (Mddct)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef DECODER_OPENVINO_ASR_MODEL_H_
 #define DECODER_OPENVINO_ASR_MODEL_H_
 
@@ -42,7 +59,6 @@ class OVAsrModel : public AsrModel {
   int cnn_module_kernel_ = 0;
   int head_ = 0;
 
-  //static std::shared_ptr<ov::Core> core_;
   std::shared_ptr<ov::Core> core_;
   // sessions
   // NOTE(Mddct): The Env holds the logging state used by all other objects.
