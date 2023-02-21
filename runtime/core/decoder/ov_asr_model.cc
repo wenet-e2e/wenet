bc2f262029c8edaf2a1206a7cfbc0409d976363d
Zhen Zhao (Fiona) <fiona.zhao@intel.com>
2023-02-21 10:07:48 +0800

    Update ov_asr_model.cc
    Add lic claim

--- runtime/core/decoder/ov_asr_model.cc
+++ runtime/core/decoder/ov_asr_model.cc
@@ -1,5 +1,21 @@
-#include "decoder/ov_asr_model.h"
+// Copyright (c) 2020 Mobvoi Inc (Binbin Zhang, Di Wu)
+//               2022 ZeXuan Li (lizexuan@huya.com)
+//                    Xingchen Song(sxc19@mails.tsinghua.edu.cn)
+//                    hamddct@gmail.com (Mddct)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+#include "decoder/ov_asr_model.h"
 #include <algorithm>
 #include <iomanip>
 #include <iostream>
