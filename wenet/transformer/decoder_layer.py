745db8271fe83514c1248059b9cf6db975b6ee95
Binbin Zhang <binbzha@qq.com>
2023-03-20 11:35:34 +0800

    [transformer] remove concat after to simplify the code flow

--- wenet/transformer/decoder_layer.py
+++ wenet/transformer/decoder_layer.py
@@ -35,10 +35,6 @@ class DecoderLayer(nn.Module):
         normalize_before (bool):
             True: use layer_norm before each sub-block.
             False: to use layer_norm after each sub-block.
-        concat_after (bool): Whether to concat attention layer's inpu
-            and output.
-            True: x -> x + linear(concat(x, att(x)))
-            False: x -> x + att(x)
     """
     def __init__(
         self,
@@ -48,7 +44,6 @@ class DecoderLayer(nn.Module):
         feed_forward: nn.Module,
         dropout_rate: float,
         normalize_before: bool = True,
-        concat_after: bool = False,
     ):
         """Construct an DecoderLayer object."""
         super().__init__()
@@ -61,13 +56,6 @@ class DecoderLayer(nn.Module):
         self.norm3 = nn.LayerNorm(size, eps=1e-5)
         self.dropout = nn.Dropout(dropout_rate)
         self.normalize_before = normalize_before
-        self.concat_after = concat_after
-        if self.concat_after:
-            self.concat_linear1 = nn.Linear(size + size, size)
-            self.concat_linear2 = nn.Linear(size + size, size)
-        else:
-            self.concat_linear1 = nn.Identity()
-            self.concat_linear2 = nn.Identity()
 
     def forward(
         self,
@@ -115,26 +103,16 @@ class DecoderLayer(nn.Module):
             residual = residual[:, -1:, :]
             tgt_q_mask = tgt_mask[:, -1:, :]
 
-        if self.concat_after:
-            tgt_concat = torch.cat(
-                (tgt_q, self.self_attn(tgt_q, tgt, tgt, tgt_q_mask)[0]), dim=-1)
-            x = residual + self.concat_linear1(tgt_concat)
-        else:
-            x = residual + self.dropout(
-                self.self_attn(tgt_q, tgt, tgt, tgt_q_mask)[0])
+        x = residual + self.dropout(
+            self.self_attn(tgt_q, tgt, tgt, tgt_q_mask)[0])
         if not self.normalize_before:
             x = self.norm1(x)
 
         residual = x
         if self.normalize_before:
             x = self.norm2(x)
-        if self.concat_after:
-            x_concat = torch.cat(
-                (x, self.src_attn(x, memory, memory, memory_mask)[0]), dim=-1)
-            x = residual + self.concat_linear2(x_concat)
-        else:
-            x = residual + self.dropout(
-                self.src_attn(x, memory, memory, memory_mask)[0])
+        x = residual + self.dropout(
+            self.src_attn(x, memory, memory, memory_mask)[0])
         if not self.normalize_before:
             x = self.norm2(x)
 
