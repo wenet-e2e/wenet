82237008671a98bc4f4cb165dab0942e0fe05d68
xiangru.meng <meng1984>
2023-08-28 17:01:38 +0800

    first commit

--- examples/aishell/s0/run.sh
+++ examples/aishell/s0/run.sh
@@ -119,7 +119,7 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
   done
 fi
 
-export CUDA_VISIBLE_DEVICES="7"
+export CUDA_VISIBLE_DEVICES="4,5,6,7"
 echo $CUDA_VISIBLE_DEVICES
 
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
