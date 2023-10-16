d99295b753f7333a70c3648ab99731ba562c404d
Binbin Zhang <binbzha@qq.com>
2023-10-16 18:36:17 +0800

    [cli] refactor cli (#2054)
    * [cli] refactor cli
    
    * fix lint

--- wenet/cli/transcribe.py
+++ wenet/cli/transcribe.py
@@ -13,16 +13,8 @@
 # limitations under the License.
 
 import argparse
-import os
 
-import torch
-import torchaudio
-import torchaudio.compliance.kaldi as kaldi
-
-from wenet.cli.hub import Hub
-from wenet.utils.common import remove_duplicates_and_blank
-from wenet.utils.file_utils import read_symbol_table
-
+from wenet.cli.model import Model
 
 def get_args():
     parser = argparse.ArgumentParser(description='')
@@ -41,28 +33,8 @@ def get_args():
 
 def main():
     args = get_args()
-    model_dir = Hub.get_model_by_lang(args.language)
-    model_path = os.path.join(model_dir, 'final.zip')
-    units_path = os.path.join(model_dir, 'units.txt')
-    model = torch.jit.load(model_path)
-    symbol_table = read_symbol_table(units_path)
-    char_dict = {v: k for k, v in symbol_table.items()}
-    waveform, sample_rate = torchaudio.load(args.audio_file, normalize=False)
-    waveform = waveform.to(torch.float)
-    feats = kaldi.fbank(waveform,
-                        num_mel_bins=80,
-                        frame_length=25,
-                        frame_shift=10,
-                        energy_floor=0.0,
-                        sample_frequency=16000)
-    feats = feats.unsqueeze(0)
-    encoder_out, _, _ = model.forward_encoder_chunk(feats, 0, -1)
-    ctc_probs = model.ctc_activation(encoder_out)
-    topk_prob, topk_index = ctc_probs.topk(1, dim=2)
-    topk_index = topk_index.squeeze().tolist()
-    hyp = remove_duplicates_and_blank(topk_index)
-    hyp = [char_dict[x] for x in hyp]
-    result = ''.join(hyp)
+    model = Model(args.language)
+    result = model.transcribe(args.audio_file)
     print(result)
 
 
