5b5ce3bba418354b158a574f64a0b30da245ced2
Binbin Zhang <binbzha@qq.com>
2023-10-30 00:07:14 +0800

    [cli] support alignment (#2088)
    * [cli] support alignment
    
    * fix compute feats

--- wenet/cli/model.py
+++ wenet/cli/model.py
@@ -19,10 +19,11 @@ import torchaudio
 import torchaudio.compliance.kaldi as kaldi
 
 from wenet.cli.hub import Hub
-from wenet.utils.ctc_utils import gen_timestamps_from_peak
+from wenet.utils.ctc_utils import (force_align, gen_ctc_peak_time,
+                                   gen_timestamps_from_peak)
 from wenet.utils.file_utils import read_symbol_table
 from wenet.transformer.search import (attention_rescoring,
-                                      ctc_prefix_beam_search)
+                                      ctc_prefix_beam_search, DecodeResult)
 
 
 class Model:
@@ -30,10 +31,10 @@ class Model:
         model_path = os.path.join(model_dir, 'final.zip')
         units_path = os.path.join(model_dir, 'units.txt')
         self.model = torch.jit.load(model_path)
-        symbol_table = read_symbol_table(units_path)
-        self.char_dict = {v: k for k, v in symbol_table.items()}
+        self.symbol_table = read_symbol_table(units_path)
+        self.char_dict = {v: k for k, v in self.symbol_table.items()}
 
-    def transcribe(self, audio_file: str, tokens_info: bool = False):
+    def compute_feats(self, audio_file: str) -> torch.Tensor:
         waveform, sample_rate = torchaudio.load(audio_file, normalize=False)
         waveform = waveform.to(torch.float)
         feats = kaldi.fbank(waveform,
@@ -43,10 +44,32 @@ class Model:
                             energy_floor=0.0,
                             sample_frequency=16000)
         feats = feats.unsqueeze(0)
+        return feats
+
+    def _decode(self,
+                audio_file: str,
+                tokens_info: bool = False,
+                label: str = None) -> dict:
+        feats = self.compute_feats(audio_file)
         encoder_out, _, _ = self.model.forward_encoder_chunk(feats, 0, -1)
         encoder_lens = torch.tensor([encoder_out.size(1)], dtype=torch.long)
         ctc_probs = self.model.ctc_activation(encoder_out)
-        ctc_prefix_results = ctc_prefix_beam_search(ctc_probs, encoder_lens, 2)
+        if label is None:
+            ctc_prefix_results = ctc_prefix_beam_search(
+                ctc_probs, encoder_lens, 2)
+        else:  # force align mode, construct ctc prefix result from alignment
+            label_t = self.tokenize(label)
+            alignment = force_align(ctc_probs.squeeze(0),
+                                    torch.tensor(label_t, dtype=torch.long))
+            peaks = gen_ctc_peak_time(alignment)
+            ctc_prefix_results = [
+                DecodeResult(tokens=label_t,
+                             score=0.0,
+                             times=peaks,
+                             nbest=[label_t],
+                             nbest_scores=[0.0],
+                             nbest_times=[peaks])
+            ]
         rescoring_results = attention_rescoring(self.model, ctc_prefix_results,
                                                 encoder_out, encoder_lens, 0.3,
                                                 0.5)
@@ -71,6 +94,27 @@ class Model:
                 })
             result['tokens'] = tokens_info
         return result
+
+    def transcribe(self, audio_file: str, tokens_info: bool = False) -> dict:
+        return self._decode(audio_file, tokens_info)
+
+    def tokenize(self, label: str):
+        # TODO(Binbin Zhang): Support BPE
+        tokens = []
+        for c in label:
+            if c == ' ':
+                c = "▁"
+            tokens.append(c)
+        token_list = []
+        for c in tokens:
+            if c in self.symbol_table:
+                token_list.append(self.symbol_table[c])
+            elif '<unk>' in self.symbol_table:
+                token_list.append(self.symbol_table['<unk>'])
+        return token_list
+
+    def align(self, audio_file: str, label: str) -> dict:
+        return self._decode(audio_file, True, label)
 
 
 def load_model(language: str = None, model_dir: str = None) -> Model:
