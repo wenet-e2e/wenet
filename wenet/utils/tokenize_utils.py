8b41eb3976e7c7a3dd4a6abfdfea154399c70a51
user01 <user01@user01deMacBook-Pro.local>
2023-11-12 20:32:29 +0800

    [cli] refactor tokenize_by_bpe_model

--- wenet/utils/tokenize_utils.py
+++ wenet/utils/tokenize_utils.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021 Mobvoi Inc. (authors: Binbin Zhang)
-#
+#               2023 Horizon Inc. (authors: Xingchen Song)
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
