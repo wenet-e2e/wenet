36af934edbb03e336ccac4c18060bd2cc088c227
Mddct <hamddct@gmail.com>
2023-12-08 13:00:21 +0800

    [text] add tries for bpe part

--- wenet/text/bpe_tokenizer.py
+++ wenet/text/bpe_tokenizer.py
@@ -2,6 +2,7 @@ from os import PathLike
 from typing import Dict, List, Optional, Union
 from wenet.text.char_tokenizer import CharTokenizer
 from wenet.text.tokenize_utils import tokenize_by_bpe_model
+from wenet.text.tries import Trie
 
 
 class BpeTokenizer(CharTokenizer):
@@ -21,6 +22,9 @@ class BpeTokenizer(CharTokenizer):
         # NOTE(Mddct): multiprocessing.Process() issues
         #              don't build sp here
         self.bpe_model = None
+        # NOTE(Mddct): we can handle proto, see:
+        # https://github.com/google/sentencepiece/issues/121#issuecomment-400362011
+        self.extra_tokens = Trie()
 
     def _build_sp(self):
         if self.bpe_model is None:
@@ -37,6 +41,13 @@ class BpeTokenizer(CharTokenizer):
         else:
             parts = [line]
 
+        extra_part = []
+        for part in parts:
+            if part in self.non_lang_syms:
+                extra_part.append(part)
+            else:
+                extra_part.extend(self.extra_tokens.split(part))
+        parts = extra_part
         tokens = []
         for part in parts:
             if part in self.non_lang_syms:
@@ -49,3 +60,13 @@ class BpeTokenizer(CharTokenizer):
         self._build_sp()
         text = super().tokens2text(tokens)
         return text.replace("▁", ' ').strip()
+
+    def add_tokens(self, tokens: List[str]) -> int:
+        added_tokens = 0
+        for token in tokens:
+            if token not in self.symbol_table:
+                self.extra_tokens.add(token)
+                self.symbol_table[token] = len(self.symbol_table)
+                added_tokens += 1
+                self.char_dict[len(self.char_dict)] = token
+        return added_tokens
