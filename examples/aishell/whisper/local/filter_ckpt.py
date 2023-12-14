10792bc138e068a483704aca505523253431e69d
xingchensong <xingchensong1996@163.com>
2023-12-14 11:44:40 +0800

    [recipe] whisper aishell

--- examples/aishell/whisper/local/filter_ckpt.py
+++ examples/aishell/whisper/local/filter_ckpt.py
@@ -11,3 +11,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import argparse
+import torch
+
+
+def main():
+    parser = argparse.ArgumentParser(description='filter out unused module')
+    parser.add_argument('--filter_list',
+                        required=True,
+                        type=str,
+                        help='list of name filter, comma-separated')
+    parser.add_argument('--input_ckpt',
+                        required=True,
+                        type=str,
+                        help='original checkpoint')
+    parser.add_argument('--output_ckpt',
+                        required=True,
+                        type=str,
+                        help='modified checkpoint')
+    args = parser.parse_args()
+
+    state = torch.load(args.input_ckpt, map_location="cpu")
+    new_state = {}
+    filter_list = args.filter_list.split(',')
+    for k in state.keys():
+        found = False
+        for prefix in filter_list:
+            if prefix in k:
+                print("skip {}".format(k))
+                found = True
+                break
+        if found:
+            continue
+        new_state[k] = state[k]
+    torch.save(new_state, args.output_ckpt)
