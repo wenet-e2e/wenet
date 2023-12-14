d39af5805cf187831303068711f0c8bb8a9ca4fe
Xingchen Song(宋星辰) <xingchensong1996@163.com>
2023-12-14 14:17:11 +0800

    [examples] whisper aishell (#2238)
    * [recipe] whisper aishell
    
    * [recipe] whisper aishell
    
    * [recipe] try to pass ut
    
    * [recipe] try to pass ut
    
    * [recipe] try to pass ut
    
    * [recipe] add original result

---
+++ examples/aishell/whisper/local/filter_ckpt.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2023 Wenet Community. (authors: Xingchen Song)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import torch
+
+
+def main():
+    parser = argparse.ArgumentParser(description='filter out unused module')
+    parser.add_argument('--filter_list',
+                        required=True,
+                        type=str,
+                        help='list of name filter, comma-separated')
+    parser.add_argument('--input_ckpt',
+                        required=True,
+                        type=str,
+                        help='original checkpoint')
+    parser.add_argument('--output_ckpt',
+                        required=True,
+                        type=str,
+                        help='modified checkpoint')
+    args = parser.parse_args()
+
+    state = torch.load(args.input_ckpt, map_location="cpu")
+    new_state = {}
+    filter_list = args.filter_list.split(',')
+    for k in state.keys():
+        found = False
+        for prefix in filter_list:
+            if prefix in k:
+                print("skip {}".format(k))
+                found = True
+                break
+        if found:
+            continue
+        new_state[k] = state[k]
+    torch.save(new_state, args.output_ckpt)
