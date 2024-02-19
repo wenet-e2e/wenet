48c83e63e97ff2bcafa8161c3d33c6293c44c4ec
Mddct <hamddct@gmail.com>
2024-02-19 21:27:25 +0800

    zero out  pad mask att

--- test/wenet/transformer/test_attention.py
+++ test/wenet/transformer/test_attention.py
@@ -5,6 +5,9 @@
 import torch
 import pytest
 from wenet.transformer.attention import MultiHeadedAttention
+from wenet.transformer.encoder_layer import TransformerEncoderLayer
+from wenet.transformer.positionwise_feed_forward import PositionwiseFeedForward
+from wenet.utils.class_utils import WENET_ACTIVATION_CLASSES
 
 from wenet.utils.mask import add_optional_chunk_mask, make_non_pad_mask
 
@@ -53,16 +56,55 @@ def test_sdpa(args):
                                        num_decoding_left_chunks=-1)
     output, cache = mha_module(q, k, v, mask=att_mask)
 
-    att_mask = (1.0 - att_mask.float()) * torch.finfo(torch.float).min
-    output_with_sdpa, cache_with_sdpa = mha_module_with_sdpa(q,
-                                                             k,
-                                                             v,
-                                                             mask=att_mask)
+    att_mask_bias = (1.0 - att_mask.float()) * torch.finfo(torch.float).min
+    output_with_sdpa, cache_with_sdpa = mha_module_with_sdpa(
+        q, k, v, mask=att_mask_bias)
 
     assert torch.allclose(
         output * mask.transpose(1, 2),
         output_with_sdpa * mask.transpose(1, 2),
         atol=9e-7,
-        rtol=9e-4,
+    )
+    assert torch.allclose(cache, cache_with_sdpa)
+
+    torch.manual_seed(777)
+    mha_layer = TransformerEncoderLayer(
+        args['n_feat'],
+        mha_module,
+        PositionwiseFeedForward(
+            args['n_feat'],
+            2048,
+            0.0,
+            WENET_ACTIVATION_CLASSES['swish'](),
+        ),
+        0.0,
+        normalize_before=True,
+    )
+
+    torch.manual_seed(777)
+    mha_layer_with_sdpa = TransformerEncoderLayer(
+        args['n_feat'],
+        mha_module_with_sdpa,
+        PositionwiseFeedForward(
+            args['n_feat'],
+            2048,
+            0.0,
+            WENET_ACTIVATION_CLASSES['swish'](),
+        ),
+        0.0,
+        normalize_before=True,
+    )
+    mha_layer.eval()
+    mha_layer_with_sdpa.eval()
+    output, _, cache, _ = mha_layer(q, att_mask, None, mask)
+    output_with_sdpa, _, cache_with_sdpa, _ = mha_layer_with_sdpa(
+        q, att_mask_bias, None, mask)
+
+    print(output)
+    print(output_with_sdpa)
+    assert torch.allclose(
+        output,
+        output_with_sdpa,
+        atol=9e-7,
     )
     assert torch.allclose(cache, cache_with_sdpa)
