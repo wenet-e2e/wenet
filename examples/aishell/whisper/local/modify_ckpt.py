c80efa84f50dcb0937ca5190f5a5b2b03d6f78c5
fclearner <614478391@qq.com>
2024-03-04 10:42:44 +0000

    fix whisper ckpt modify error

--- examples/aishell/whisper/local/modify_ckpt.py
+++ examples/aishell/whisper/local/modify_ckpt.py
@@ -20,13 +20,13 @@ import json
 def main():
     parser = argparse.ArgumentParser(description='filter out unused module')
     parser.add_argument('--remove_list',
-                        default="name1,name2",
+                        default="",
                         type=str,
-                        help='list of name filter, comma-separated')
+                        help='list of name filter, comma-separated, e.g."name1, name2"')
     parser.add_argument('--add_list',
-                        default="{\"key1\": \"value1\"}",
+                        default="",
                         type=str,
-                        help='dict of name adder')
+                        help='dict of name adder, e.g."{\"key1\": \"value1\"}"')
     parser.add_argument('--input_ckpt',
                         required=True,
                         type=str,
@@ -39,21 +39,24 @@ def main():
 
     state = torch.load(args.input_ckpt, map_location="cpu")
     new_state = {}
-    remove_list = args.remove_list.split(',')
-    for k in state.keys():
-        found = False
-        for prefix in remove_list:
-            if prefix in k:
-                print("skip {}".format(k))
-                found = True
-                break
-        if found:
-            continue
-        new_state[k] = state[k]
-    add_list = json.loads(args.add_list)
-    for k, v in add_list.items():
-        print("init {} with {}".format(k, v))
-        new_state[k] = state[v]
+    if args.remove_list:
+        remove_list = args.remove_list.split(',')
+        for k in state.keys():
+            found = False
+            for prefix in remove_list:
+                if prefix in k:
+                    print("skip {}".format(k))
+                    found = True
+                    break
+            if found:
+                continue
+            new_state[k] = state[k]
+
+    if args.add_list:
+        add_list = json.loads(args.add_list)
+        for k, v in add_list.items():
+            print("init {} with {}".format(k, v))
+            new_state[k] = state[v]
     torch.save(new_state, args.output_ckpt)
 
 
