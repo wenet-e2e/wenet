9ae1562ad64253159cee2bed63f1bab5a5408194
Dinghao Zhou <hamddct@gmail.com>
2024-03-18 13:44:56 +0800

    rm self.step

--- wenet/utils/executor.py
+++ wenet/utils/executor.py
@@ -103,7 +103,7 @@ class Executor:
                 log_per_step(writer, info_dict, timer=self.train_step_timer)
                 self.step += 1 if (batch_idx +
                                    1) % info_dict["accum_grad"] == 0 else 0
-                self.train_step_timer.step = self.step
+
 
     def cv(self, model, cv_data_loader, configs):
         ''' Cross validation on
