5676e0bf0c5c97c82e0d86889cf2be747a38d22d
Mddct <hamddct@gmail.com>
2024-04-03 18:35:59 +0800

    llama rope and google rope work!

--- wenet/utils/rope_utils.py
+++ wenet/utils/rope_utils.py
@@ -29,9 +29,10 @@ def google_apply_rotary_emb(x: torch.Tensor,
 def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
     ndim = x.ndim
     assert 0 <= 1 < ndim
-    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    assert freqs_cis.shape[2:] == (x.shape[1], x.shape[-1])
+    # 2 is seq_len in wenet
     shape = [
-        d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)
+        d if i == 2 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)
     ]
     return freqs_cis.view(*shape)
 
