c415f6cf744e4959ed1c76a9dd8988ba3eedc412
Xingchen Song(宋星辰) <xingchensong1996@163.com>
2024-04-18 12:18:16 +0800

    [log] avoid reduntant logging (#2493)

--- wenet/transformer/encoder.py
+++ wenet/transformer/encoder.py
@@ -190,9 +190,12 @@ class BaseEncoder(torch.nn.Module):
                                     pos_emb: torch.Tensor,
                                     mask_pad: torch.Tensor) -> torch.Tensor:
         for layer in self.encoders:
-            xs, chunk_masks, _, _ = ckpt.checkpoint(layer.__call__, xs,
-                                                    chunk_masks, pos_emb,
-                                                    mask_pad, use_reentrant=False)
+            xs, chunk_masks, _, _ = ckpt.checkpoint(layer.__call__,
+                                                    xs,
+                                                    chunk_masks,
+                                                    pos_emb,
+                                                    mask_pad,
+                                                    use_reentrant=False)
         return xs
 
     def forward_chunk(
