f7417593445a6b80597e185340bd90be643ce70f
park sang-hoon <timesea821@naver.com>
2024-04-30 16:26:28 +0900

    8k and 16k support

--- runtime/core/websocket/websocket_client.h
+++ runtime/core/websocket/websocket_client.h
@@ -32,7 +32,7 @@ namespace wenet {
 namespace beast = boost::beast;          // from <boost/beast.hpp>
 namespace http = beast::http;            // from <boost/beast/http.hpp>
 namespace websocket = beast::websocket;  // from <boost/beast/websocket.hpp>
-namespace asio = boost::asio;            // from <boost/asio.hpp> or
+namespace asio = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;        // from <boost/asio/ip/tcp.hpp>
 
 class WebSocketClient {
