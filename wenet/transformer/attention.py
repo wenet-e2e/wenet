27d06f0e4754a4e5a72f529179b3280ba947bf31
Mddct <hamddct@gmail.com>
2024-06-03 21:24:39 +0800

    [transformer] w2vbert attention reduce memory

--- wenet/transformer/attention.py
+++ wenet/transformer/attention.py
@@ -530,9 +530,11 @@ class ShawRelPositionMultiHeadedAttention(MultiHeadedAttention):
         self.rel_k_embed = torch.nn.Embedding(
             self.max_left_rel_pos + self.max_right_rel_pos + 1, self.d_k)
 
-    def _relative_indices(self, length: int, device: torch.device):
-        indices = torch.arange(length, device=device).unsqueeze(0)
-        rel_indices = indices - indices.transpose(0, 1)
+    def _relative_indices(self, query_length: int, key_length: int,
+                          device: torch.device):
+        l_indices = torch.arange(query_length, device=device).view(-1, 1)
+        r_indices = torch.arange(key_length, device=device).view(1, -1)
+        rel_indices = l_indices - r_indices
         rel_indices = torch.clamp(rel_indices, -self.max_left_rel_pos,
                                   self.max_right_rel_pos)
         return rel_indices + self.max_left_rel_pos
@@ -551,13 +553,15 @@ class ShawRelPositionMultiHeadedAttention(MultiHeadedAttention):
         k, v, new_cache = self._update_kv_and_cache(k, v, cache)
 
         rel_k = self.rel_k_embed(
-            self._relative_indices(k.size(2), query.device))  # (t2, t2, d_k)
-        rel_k = rel_k[-q.size(2):]  # (t1, t2, d_k)
-        # b,h,t1,dk
-        rel_k = rel_k.unsqueeze(0).unsqueeze(0)  # (1, 1, t1, t2, d_k)
-        q_expand = q.unsqueeze(3)  # (batch, h, t1, 1, d_k)
-        rel_att_weights = (rel_k * q_expand).sum(-1).squeeze(
-            -1)  # (batch, h, t1, t2)
+            self._relative_indices(q.size(2), k.size(2),
+                                   query.device))  # (t2, t2, d_k)
+        # rel_k = rel_k[-q.size(2):]  # (t1, t2, d_k)
+        # # b,h,t1,dk
+        # rel_k = rel_k.unsqueeze(0).unsqueeze(0)  # (1, 1, t1, t2, d_k)
+        # q_expand = q.unsqueeze(3)  # (batch, h, t1, 1, d_k)
+        # rel_att_weights = (rel_k * q_expand).sum(-1).squeeze(
+        #     -1)  # (batch, h, t1, t2)
+        rel_att_weights = torch.einsum("bhld,lrd->bhlr", q, rel_k)
 
         if not self.use_sdpa:
             scores = (torch.matmul(q, k.transpose(-2, -1)) +
