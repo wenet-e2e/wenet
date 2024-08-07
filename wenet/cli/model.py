98eac6f1e988eb4868a0f63615ed42b145b3bb3f
彭震东 <275331498@qq.com>
2024-08-07 16:46:38 +0800

    [utils] use force_align of torchaudio (#2597)
    * [utils] use force_align of torchaudio
    
    * rounded outputs of force_align
    
    * remove empty line

--- wenet/cli/model.py
+++ wenet/cli/model.py
@@ -128,9 +128,9 @@ class Model:
             for i, x in enumerate(res.tokens):
                 tokens_info.append({
                     'token': self.char_dict[x],
-                    'start': times[i][0],
-                    'end': times[i][1],
-                    'confidence': res.tokens_confidence[i]
+                    'start': round(times[i][0], 3),
+                    'end': round(times[i][1], 3),
+                    'confidence': round(res.tokens_confidence[i], 2)
                 })
             result['tokens'] = tokens_info
         return result
