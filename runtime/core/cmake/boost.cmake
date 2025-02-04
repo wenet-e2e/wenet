58716bfc85570ebebb0e48df2aae5c5a78b9ef05
Sang-Hoon Park <timesea821@naver.com>
2025-02-04 17:35:30 +0900

    Update boost.cmake

--- runtime/core/cmake/boost.cmake
+++ runtime/core/cmake/boost.cmake
@@ -1,5 +1,5 @@
 FetchContent_Declare(boost
-  URL      https://boostorg.jfrog.io/artifactory/main/release/1.75.0/source/boost_1_75_0.tar.gz
+  URL      https://archives.boost.io/release/1.75.0/source/boost_1_75_0.tar.gz
   URL_HASH SHA256=aeb26f80e80945e82ee93e5939baebdca47b9dee80a07d3144be1e1a6a66dd6a
 )
 FetchContent_MakeAvailable(boost)
