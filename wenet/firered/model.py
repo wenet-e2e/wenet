03a527e4c58a6b44cf1370d85dbd0e879a9d6fba
Mddct <hamddct@gmail.com>
2025-02-06 21:05:02 +0800

    fix conflict

--- wenet/firered/model.py
+++ wenet/firered/model.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Modified from [Whisper](https://github.com/openai/whisper)
 
 from typing import Optional, Tuple
 
