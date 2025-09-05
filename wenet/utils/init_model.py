d50ba36b46a2adf25be4fae82fc8b9009b8997e8
Binbin Zhang <binbzha@qq.com>
2025-09-05 09:19:48 +0000

    fix lint

--- wenet/utils/init_model.py
+++ wenet/utils/init_model.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 
 import torch
 from wenet.branchformer.encoder import BranchformerEncoder
